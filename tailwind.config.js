0fb327a687f44e1b502417ab03cd32f61f049fe0
srihari996 <srihari521343@gmail.com>
2023-01-10 22:43:20 +0530

    Added Herocarousel and pending changes in Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBackground: {
+          50: "#edf1fc",
+          100: "#d3d4e1",
+          200: "#b6b8c9",
+          300: "#989bb2",
+          400: "#7c7f9b",
+          500: "#636582",
+          600: "#4c4f66",
+          700: "#363849",
+          800: "#21222e",
+          900: "#0a0a16",
+        },
+        premier:{
+          50: "#edf1fc",
+          100: "#d3d4e1",
+          200: "#b6b8c9",
+          300: "#989bb2",
+          400: "#7c7f9b",
+          500: "#636582",
+          600: "#4c4f66",
+          700: "#363849",
+          800: "#21222e",
+          900: "#0a0a16",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
